bf6379eddd48fda8d4cefe03a4ce0e5245ca330d
Andrew Morrow <amorrow1991@gmail.com>
2020-10-02 23:56:47 -0500

    makes sure you get one of each selected character in the password

--- javaScript/script.js
+++ javaScript/script.js
@@ -5,6 +5,8 @@ let generateBtn = document.querySelector("#generate");
 var possibleChars = [];
 // initializes passLength variable
 var passLength;
+var password = "";
+var numberSelected = 0;
 // array of lowercase alphabetical possibilities
 var alphabeticalLowerChar = [
     "a",
@@ -111,6 +113,7 @@ generateBtn.addEventListener("click", userPrompts);
 // function that runs all of the user prompts when the generate password button is clicked
 function userPrompts() {
     possibleChars = [];
+    password = "";
     alert(
         `The next few prompts will gather your requirements for your password.
 The "OK" button will select yes and the "Cancel" button will select no.`
@@ -151,18 +154,36 @@ Click "OK" for yes or "Cancel" for no`
     }
     // this adds the special characters to the array if the user selected the yes option
     if (includeSpecial) {
+        passLength--;
+        // this guarantees at least one special character will be in password if selected
+        password += specialChar[Math.floor(Math.random() * specialChar.length)];
         possibleChars = possibleChars.concat(specialChar);
     }
     // this adds the lowercase characters to the array if the user selected the yes option
     if (includeLower) {
+        passLength--;
+        // this guarantees at least one lowercase will be in password if selected
+        password +=
+            alphabeticalLowerChar[
+                Math.floor(Math.random() * alphabeticalLowerChar.length)
+            ];
         possibleChars = possibleChars.concat(alphabeticalLowerChar);
     }
     // this adds the uppercasecharacters to the array if the user selected the yes option
     if (includeUpper) {
+        passLength--;
+        // this guarantees at least one uppercase will be in password if selected
+        password +=
+            alphabeticalUpperChar[
+                Math.floor(Math.random() * alphabeticalUpperChar.length)
+            ];
         possibleChars = possibleChars.concat(alphabeticalUpperChar);
     }
     // this adds the uppercasecharacters to the array if the user selected the yes option
     if (includeNumeric) {
+        passLength--;
+        // this guarantees at least one numeric will be in password if selected
+        password += numeric[Math.floor(Math.random() * numeric.length)];
         possibleChars = possibleChars.concat(numeric);
     }
     // this calls the writePassword function to display to the password id box
@@ -171,19 +192,18 @@ Click "OK" for yes or "Cancel" for no`
 
 //this function generates the password using the Math.random function and concatenating the selected requirements arrays
 function generatePassword() {
-    var password = "";
     for (i = 0; i < passLength; i++) {
         password +=
             possibleChars[Math.floor(possibleChars.length * Math.random())];
     }
-
+    console.log(password);
     return password;
 }
 
 // Writes password to the #password text field
 function writePassword() {
     // this runs the function and sets the returned generated password in a variable
-    var password = generatePassword();
+    password = generatePassword();
     // this targets the textarea and sets a variable for it
     var passwordText = document.querySelector("#password");
     // this sets the textarea variable to equal the password variable
